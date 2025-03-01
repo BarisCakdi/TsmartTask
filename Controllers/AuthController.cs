e06631c543c17375dc575a45ce27c9935b6940d2
Baris <bariscakdi@gmail.com>
2025-03-02 02:22:46 +0300

    Jwt kontrollü eklendi

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TsmartTask.Data;
+using TsmartTask.DTOs;
+using TsmartTask.Model;
+using TsmartTask.Services;
 
 namespace TsmartTask.Controllers
 {
@@ -7,6 +11,26 @@ namespace TsmartTask.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly AppDbContext _context;
+        private readonly IJwtService _jwtService;
+        public AuthController(AppDbContext context, IJwtService jwtService)
+        {
+            _context = context;
+            _jwtService = jwtService;
+        }
 
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] dtoLoginRequest request)
+        {
+            var user = new User() { Id = 3, Username = "", Password = "" };
+
+            if (user == null)
+            {
+                return Unauthorized(new { message = "Geçersiz kullanıcı adı veya şifre." });
+            }
+
+            var token = _jwtService.GenerateToken(user);
+            return Ok(new { token });
+        }
     }
 }
