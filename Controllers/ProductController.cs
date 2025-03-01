358543befc661e199b3f06097c782aa8f507b1ca
Baris <bariscakdi@gmail.com>
2025-03-02 02:24:46 +0300

    Product listeleme ve id göre ürün getirme eklendi

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TsmartTask.Data;
+using TsmartTask.Model;
+
+namespace TsmartTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Product> GetAll()
+        {
+            var products = _context.Products.ToList();
+
+            return products;
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound("Ürün Bulunamadı");
+            }
+            return Ok(product);
+        }
+    }
+}
